04cfc964233d502ed91f66c5911dde7eba1c7a39
Rodrigo <rodrigo6802@ufpi.edu.br>
2023-04-06 22:20:42 -0300

    Insercao Pipeline Cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+describe('BD_CONNECT', () => {
+   
+    it('Funerario Santa Luzia Sua Morte nossa alegria',()=>{
+        cy.visit("https://paxuniao.com/")
+
+
+
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('.nav > :nth-child(3) > a').click();
+        cy.get('#cn-notice-buttons > [data-cookie-set="accept"]').click();
+        /* ==== End Cypress Studio ==== */
+    })
+
+    
+})

3cf72d013334b8ab93d4acccc8e7b1a24d717fe9
Rodrigo Eduardo <89258381+souloucopo@users.noreply.github.com>
2023-04-06 22:36:46 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
+    projectId: "593m91",
     experimentalStudio: true,
     setupNodeEvents(on, config) {
 
